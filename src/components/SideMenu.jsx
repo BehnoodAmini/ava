8eb62c66183bda954c60c80266fbd88a2eaa2b64
Your Name <behnooda81@gmail.com>
2023-07-27 13:19:23 +0330

    firstCommit

---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import "./SideMenu.css";
+
+import alefba from '../assets/images/alefba-group-1.png';
+import archiveIcon from '../assets/images/archive-icon-1@2x.png';
+import speechIcon from '../assets/images/speech-icon-1@2x.png';
+import line1Icon from '../assets/images/line-1-1.svg';
+import line2Icon from '../assets/images/line-2-1.svg';
+
+const SideMenu = ({ className }) => {
+  return (
+    <div className={`side-menu ${className}`}>
+      <div className="overlap">
+        <div className="back">
+          <div className="overlap-group">
+            <div className="div" />
+            <img
+              className="alefba-group"
+              alt="Alefba group"
+              src={alefba}
+            />
+          </div>
+        </div>
+        <div className="site-title">
+          <div className="element">آوا</div>
+          <div className="icon">
+            <img
+              className="line"
+              alt="Line"
+              src={line1Icon}
+            />
+            <img
+              className="img"
+              alt="Line"
+              src={line2Icon}
+            />
+            <img
+              className="line-2"
+              alt="Line"
+              src={line2Icon}
+            />
+          </div>
+        </div>
+        <button className="goftar">
+            <div className="text-wrapper">تبدیل گفتار</div>
+            <img
+              className="speech-icon"
+              alt="Speech icon"
+              src={speechIcon}
+            />
+        </button>
+        <button className="archive">
+          <img
+            className="archive-icon"
+            alt="Archive icon"
+            src={archiveIcon}
+          />
+          <div className="text-wrapper">آرشیو</div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SideMenu;
