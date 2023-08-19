3c0a41951f90084585ec244c1d913167c6c1e1d3
Your Name <behnooda81@gmail.com>
2023-08-19 15:40:43 +0330

    Refactor Upload component

--- src/components/upload/Upload.jsx
+++ src/components/upload/Upload.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useRef } from "react";
 
 import AudioUploaded from "../AudioUploaded/AudioUploaded";
+import RecordVoice from "./recordVoiceAndUpload/RecordVoiceAndUpload";
 import LanguageDropdown from "../dropdowns/LanguageDropdown";
 import "./Upload.css";
 
@@ -12,23 +13,15 @@ import uploadIconGray from "../../assets/images/upload-icon-gray.svg";
 import linkIconWhite from "../../assets/images/chain-icon-white.svg";
 import linkIconGray from "../../assets/images/chain-icon-gray.svg";
 
-const mimeType = "audio/webm";
-
 const Upload = (props) => {
 
     const [isShownRecord, setIsShownRecord] = useState(true);
     const [isShownUpload, setIsShownUpload] = useState(false);
     const [isShownLink, setIsShownLink] = useState(false);
     const [fileAudio, setFileAudio] = useState(false);
-    //const [isUploaded, setIsUploaded] = useState(false)
-
-    // for RECORDING
-    const [permission, setPermission] = useState(false);
-    const mediaRecorder = useRef(null);
-    const [recordingStatus, setRecordingStatus] = useState("inactive");
-    const [stream, setStream] = useState(null);
+    
+    // STATES FOR AUDIO
     const [audio, setAudio] = useState(null);
-    const [audioChunks, setAudioChunks] = useState([]);
     const [duration, setDuration] = useState(0);
     const audioRef = useRef(null);
 
@@ -58,73 +51,6 @@ const Upload = (props) => {
         if (!isShownLink) {
             setFileAudio(false);
         }
-    };
-
-    // FOR RECORDING
-    const getMicrophonePermission = async () => {
-        if ("MediaRecorder" in window) {
-            try {
-                const mediaStream = await navigator.mediaDevices.getUserMedia({
-                    audio: true,
-                    video: false,
-                });
-                setPermission(true);
-                setStream(mediaStream);
-            } catch (err) {
-                alert(err.message);
-            }
-        } else {
-            alert("The MediaRecorder API is not supported in your browser.");
-        }
-    };
-
-    const startRecording = async () => {
-        setRecordingStatus("recording");
-        const media = new MediaRecorder(stream, { type: mimeType });
-
-        mediaRecorder.current = media;
-
-        mediaRecorder.current.start();
-
-        let localAudioChunks = [];
-
-        mediaRecorder.current.ondataavailable = (event) => {
-            if (typeof event.data === "undefined") return;
-            if (event.data.size === 0) return;
-            localAudioChunks.push(event.data);
-        };
-
-        setAudioChunks(localAudioChunks);
-    };
-
-    const stopRecording = () => {
-        setRecordingStatus("inactive");
-        mediaRecorder.current.stop();
-
-        mediaRecorder.current.onstop = () => {
-            const audioBlob = new Blob(audioChunks, { type: mimeType });
-            const audioUrl = URL.createObjectURL(audioBlob);
-
-            setAudio(audioUrl);
-
-            setAudioChunks([]);
-
-            setFileAudio(true);
-
-            // FOR DURATION OF RECORDED VOICE
-            const fileReader = new FileReader();
-            fileReader.onloadend = () => {
-                const audioContext = new window.AudioContext();
-                const arrayBuffer = fileReader.result;
-
-                audioContext.decodeAudioData(arrayBuffer, (buffer) => {
-                    const durationInSeconds = buffer.duration;
-                    setDuration(durationInSeconds);
-                });
-            };
-
-            fileReader.readAsArrayBuffer(audioBlob);
-        };
     };
 
     //  FOR USING A BUTTON AS AN FILE INPUT
@@ -188,47 +114,16 @@ const Upload = (props) => {
                 </div>
 
                 {isShownRecord && (
-                    fileAudio
-                        ? <div><AudioUploaded
-                            isShownRecord={isShownRecord}
-                            setFileAudio={setFileAudio}
-                            color="#00BA9F"
-                            audio={audio}
-                            duration={duration}
-                            audioRef={audioRef}
-                        /></div>
-                        : (
-                            <div className="center-mic">
-                                {!permission ? (
-                                    <button className="permission" onClick={getMicrophonePermission}>
-                                        به میکروفون اجازه دسترسی دهید.<br />
-                                        (اگر قبلا اجازه داده اید فقط کلیک کنید!)
-                                    </button>
-                                ) : null}
-                                {permission && recordingStatus === "inactive" ? (
-                                    <button className="center-mic-icon" onClick={startRecording}>
-                                        <img
-                                            className="center-micIcon"
-                                            src={micIconWhite}
-                                            alt="micIcon"
-                                        />
-                                    </button>
-                                ) : null}
-                                {recordingStatus === "recording" ? (
-                                    <button className="center-mic-icon-recording" onClick={stopRecording}>
-                                        <img
-                                            className="center-micIcon"
-                                            src={micIconWhite}
-                                            alt="micIcon"
-                                        />
-                                    </button>
-                                ) : null}
-                                <div className="center-mic-text">
-                                    برای شروع به صحبت، دکمه را فشار دهید <br />
-                                    متن پیاده شده آن، در اینجا ظاهر شود
-                                </div>
-                            </div>
-                        )
+                    <RecordVoice
+                        isShownRecord={isShownRecord}
+                        fileAudio={fileAudio}
+                        setFileAudio={setFileAudio}
+                        audio={audio} 
+                        setAudio={setAudio}
+                        duration={duration}
+                        setDuration={setDuration}
+                        audioRef={audioRef}
+                        />
                 )}
 
                 {isShownUpload && (
---
+++ src/components/upload/recordVoiceAndUpload/RecordVoiceAndUpload.jsx
@@ -0,0 +1,139 @@
+import { useState, useRef } from "react";
+
+import AudioUploaded from "../../AudioUploaded/AudioUploaded";
+import './RecordVoiceAndUpload.css';
+
+import micIconWhite from "../../../assets/images/mic-icon-white.svg";
+
+const mimeType = "audio/webm";
+
+const RecordVoiceAndUpload = (
+    { isShownRecord,
+        fileAudio,
+        setFileAudio,
+        audio,
+        setAudio,
+        duration,
+        setDuration,
+        audioRef
+    }) => {
+
+    const [permission, setPermission] = useState(false);
+    const mediaRecorder = useRef(null);
+    const [recordingStatus, setRecordingStatus] = useState("inactive");
+    const [stream, setStream] = useState(null);
+    const [audioChunks, setAudioChunks] = useState([]);
+
+    //GETTING RECORDING PERMISSION FOR DOMAIN IN BROWSER
+    const getMicrophonePermission = async () => {
+        if ("MediaRecorder" in window) {
+            try {
+                const mediaStream = await navigator.mediaDevices.getUserMedia({
+                    audio: true,
+                    video: false,
+                });
+                setPermission(true);
+                setStream(mediaStream);
+            } catch (err) {
+                alert(err.message);
+            }
+        } else {
+            alert("The MediaRecorder API is not supported in your browser.");
+        }
+    };
+
+    const startRecording = async () => {
+        setRecordingStatus("recording");
+        const media = new MediaRecorder(stream, { type: mimeType });
+
+        mediaRecorder.current = media;
+
+        mediaRecorder.current.start();
+
+        let localAudioChunks = [];
+
+        mediaRecorder.current.ondataavailable = (event) => {
+            if (typeof event.data === "undefined") return;
+            if (event.data.size === 0) return;
+            localAudioChunks.push(event.data);
+        };
+
+        setAudioChunks(localAudioChunks);
+    };
+
+    const stopRecording = () => {
+        setRecordingStatus("inactive");
+        mediaRecorder.current.stop();
+
+        mediaRecorder.current.onstop = () => {
+            const audioBlob = new Blob(audioChunks, { type: mimeType });
+            const audioUrl = URL.createObjectURL(audioBlob);
+
+            setAudio(audioUrl);
+
+            setAudioChunks([]);
+
+            setFileAudio(true);
+
+            // FOR DURATION OF RECORDED VOICE
+            const fileReader = new FileReader();
+            fileReader.onloadend = () => {
+                const audioContext = new window.AudioContext();
+                const arrayBuffer = fileReader.result;
+
+                audioContext.decodeAudioData(arrayBuffer, (buffer) => {
+                    const durationInSeconds = buffer.duration;
+                    setDuration(durationInSeconds);
+                });
+            };
+
+            fileReader.readAsArrayBuffer(audioBlob);
+        };
+    };
+
+    return (
+        fileAudio
+            ? <div><AudioUploaded
+                isShownRecord={isShownRecord}
+                setFileAudio={setFileAudio}
+                audio={audio}
+                duration={duration}
+                audioRef={audioRef}
+                color="#00BA9F"
+            /></div>
+            : (
+                <div className="center-mic">
+                    {!permission ? (
+                        <button className="permission" onClick={getMicrophonePermission}>
+                            به میکروفون اجازه دسترسی دهید.<br />
+                            (اگر قبلا اجازه داده اید فقط کلیک کنید!)
+                        </button>
+                    ) : null}
+                    {permission && recordingStatus === "inactive" ? (
+                        <button className="center-mic-icon" onClick={startRecording}>
+                            <img
+                                className="center-micIcon"
+                                src={micIconWhite}
+                                alt="micIcon"
+                            />
+                        </button>
+                    ) : null}
+                    {recordingStatus === "recording" ? (
+                        <button className="center-mic-icon-recording" onClick={stopRecording}>
+                            <img
+                                className="center-micIcon"
+                                src={micIconWhite}
+                                alt="micIcon"
+                            />
+                        </button>
+                    ) : null}
+                    <div className="center-mic-text">
+                        برای شروع به صحبت، دکمه را فشار دهید <br />
+                        متن پیاده شده آن، در اینجا ظاهر شود
+                    </div>
+                </div>
+            )
+    );
+}
+
+export default RecordVoiceAndUpload;
