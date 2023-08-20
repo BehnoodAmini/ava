5dbd61b43d8eb6c891064f7555eee19c97b712af
Your Name <behnooda81@gmail.com>
2023-08-20 16:24:39 +0330

    Lazy added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
 
-import MainPage from './components/MainPage/MainPage';
-import SecondaryPage from './components/SecondaryPage/SecondaryPage';
+const MainPage = lazy(() => import('./components/MainPage/MainPage'));
+const SecondaryPage = lazy(() => import('./components/SecondaryPage/SecondaryPage'));
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route
-          path='/'
-          element={<MainPage />}
-        />
-        <Route
-          path='/archive'
-          element={<SecondaryPage />}
-        />
-      </Routes>
-      {/*<MainPage />
-      <SecondaryPage />*/}
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Suspense >
+                <MainPage />
+              </Suspense>
+            }
+          />
+
+          <Route
+            path='/archive'
+            element={
+              <Suspense>
+                <SecondaryPage />
+              </Suspense>
+            }
+          />
+        </Routes>
     </div>
   );
 }
