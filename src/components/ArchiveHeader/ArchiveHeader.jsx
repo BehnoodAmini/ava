4aabe992ad5bd68faa1de23e0ecf4907ee382292
Behnood Mohammad Amini <112243597+BehnoodAmini@users.noreply.github.com>
2023-08-22 23:21:42 +0330

    Fixed problems (#3)

--- src/components/ArchiveHeader/ArchiveHeader.jsx
+++ src/components/ArchiveHeader/ArchiveHeader.jsx
@@ -5,10 +5,12 @@ const ArchiveHeader = () => {
         <div className="archive-header">
             <h2 className="archive-title">آرشیو من</h2>
             <div className="archive-lables">
+                <div className="first-blank"></div>
                 <div className="file-name">نام فایل</div>
                 <div className="date">تاریخ بارگذاری</div>
                 <div className="data-type">نوع فایل</div>
                 <div className="file-length">مدت زمان</div>
+                <div className="last-blank"></div>
             </div>
         </div>
      );
