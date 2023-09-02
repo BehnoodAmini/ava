7cff941d3834e45dab16a4a3c9c637779626a55f
Your Name <behnooda81@gmail.com>
2023-09-03 01:15:03 +0330

    Display each archive file data that given from api

--- src/components/ArchiveFileAudio/ArchiveFileAudio.jsx
+++ src/components/ArchiveFileAudio/ArchiveFileAudio.jsx
@@ -1,9 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./ArchiveFileAudio.css"
 
 import VoiceBar from "../voiceBar/VoiceBar";
-import { testList } from "./testData";
 
 import SimpleBarReact from "simplebar-react";
 import "../../../node_modules/simplebar-react/dist/simplebar.min.css";
@@ -19,6 +18,8 @@ const ArchiveFileAudio = (props) => {
     const [simpleIsShown, setSimpleIsShown] = useState(true);
     const [timeIsShown, setTimeIsShown] = useState(false);
 
+    const [dataText, setDataText] = useState(props.textDataFromApi);
+
     const handleClickSimple = event => {
         setSimpleIsShown(true);
         setTimeIsShown(false);
@@ -28,90 +29,117 @@ const ArchiveFileAudio = (props) => {
         setTimeIsShown(true);
     }
 
-    function formatDuration(value) {
-        const minute = Math.floor(value / 60);
-        const secondLeft = value - minute * 60;
-        return `${minute < 10 ? `0${minute}` : minute}:${secondLeft < 10 ? `0${secondLeft}` : secondLeft}`;
-    }
+    const formatDuration = (value) => {
+        const timeIndex = value.split(":");
+        const hours = parseInt(timeIndex[0]);
+        const minutes = parseInt(timeIndex[1]);
+        const seconds = parseInt(timeIndex[2]);
+
+        if (hours === 0) {
+            let time = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+            return time;
+        } else {
+            let time = `${hours}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+            return time;
+        }
+    };
+
+    useEffect(() => {
+        setDataText(props.textDataFromApi);
+    }, [props.textDataFromApi]);
 
     return (
         <div className="archive-center-uploaded">
             <div className="archive-upload-navbar">
-                    <button
-                        className="archive-simple-text-button"
-                        onClick={handleClickSimple}
-                        style={simpleIsShown ? { borderBottomColor: "black" } : { borderColor: "transparent" }}
-                    >
-                        {simpleIsShown ?
-                            (
-                                <>
-                                    <img
-                                        className="simple-text-icon"
-                                        src={simpleTextIconBlack}
-                                        alt="simple-text-icon"
-                                    />
-                                    <div className="simple-text" style={{ fontWeight: "bold" }}>متن ساده</div>
-                                </>
-                            )
-                            : (
-                                <>
-                                    <img
-                                        className="simple-text-icon"
-                                        src={simpleTextIconGray}
-                                        alt="simple-text-icon"
-                                    />
-                                    <div className="simple-text" style={{ fontWeight: "lighter" }}>متن ساده</div>
-                                </>
-                            )
-                        }
-                    </button>
-                    <button
-                        className="timed-text-button"
-                        onClick={handleClickTimed}
-                        style={timeIsShown ? { borderBottomColor: "black" } : { borderColor: "transparent" }}
-                    >
-                        {timeIsShown ?
-                            (
-                                <>
-                                    <img
-                                        className="timed-text-icon"
-                                        src={timedTextIconBlack}
-                                        alt="timed-text-icon"
-                                    />
-                                    <div className="timed-text" style={{ fontWeight: "bold" }}>متن زمان بندی شده</div>
-                                </>
-                            )
-                            : (
-                                <>
-                                    <img
-                                        className="timed-text-icon"
-                                        src={timedTextIconGray}
-                                        alt="timed-text-icon"
-                                    />
-                                    <div className="timed-text" style={{ fontWeight: "lighter" }}>متن زمان بندی شده</div>
-                                </>
-                            )
-                        }
-                    </button>
+                <button
+                    className="archive-simple-text-button"
+                    onClick={handleClickSimple}
+                    style={simpleIsShown ? { borderBottomColor: "black" } : { borderColor: "transparent" }}
+                >
+                    {simpleIsShown ?
+                        (
+                            <>
+                                <img
+                                    className="simple-text-icon"
+                                    src={simpleTextIconBlack}
+                                    alt="simple-text-icon"
+                                />
+                                <div className="simple-text" style={{ fontWeight: "bold" }}>متن ساده</div>
+                            </>
+                        )
+                        : (
+                            <>
+                                <img
+                                    className="simple-text-icon"
+                                    src={simpleTextIconGray}
+                                    alt="simple-text-icon"
+                                />
+                                <div className="simple-text" style={{ fontWeight: "lighter" }}>متن ساده</div>
+                            </>
+                        )
+                    }
+                </button>
+                <button
+                    className="timed-text-button"
+                    onClick={handleClickTimed}
+                    style={timeIsShown ? { borderBottomColor: "black" } : { borderColor: "transparent" }}
+                >
+                    {timeIsShown ?
+                        (
+                            <>
+                                <img
+                                    className="timed-text-icon"
+                                    src={timedTextIconBlack}
+                                    alt="timed-text-icon"
+                                />
+                                <div className="timed-text" style={{ fontWeight: "bold" }}>متن زمان بندی شده</div>
+                            </>
+                        )
+                        : (
+                            <>
+                                <img
+                                    className="timed-text-icon"
+                                    src={timedTextIconGray}
+                                    alt="timed-text-icon"
+                                />
+                                <div className="timed-text" style={{ fontWeight: "lighter" }}>متن زمان بندی شده</div>
+                            </>
+                        )
+                    }
+                </button>
             </div>
             <img className="hr" src={hr} alt="hr" />
 
 
-            <SimpleBarReact style={{ maxHeight: 150, direction: 'rtl', scrollbarMinSize: 1, marginTop: -10 }} data-simplebar-direction='rtl' >
+            <SimpleBarReact
+                style={{
+                    maxHeight: 150,
+                    direction: 'rtl',
+                    scrollbarMinSize: 1,
+                    marginTop: -10
+                }}
+                data-simplebar-direction='rtl'
+            >
                 <div className="archive-center-box">
                     {simpleIsShown
-                        ? (<div className="text-box"></div>)
+                        ? (<div className={props.language === "fa" ? "text-box-fa" : "text-box-en"}>
+                            {dataText.map((data, key) => (
+                                <span key={key}>
+                                    {data.text}{" "}
+                                </span>
+                            ))}
+                        </div>)
                         : (
-                            testList.map((data, key) => {
+                            dataText.map((data, key) => {
                                 return (
                                     <div
                                         className="data-row"
                                         key={key}
                                         style={key % 2 === 0 ? { backgroundColor: "rgb(242, 242, 242)" } : { backgroundColor: "#ffffff" }}
                                     >
-                                        <div className="end-time">{formatDuration(data.endTime)}</div>
-                                        <div className="start-time">{formatDuration(data.startTime)}</div>
-                                        <div className="data-text">{data.text}</div>
+                                        <div className="end-time">{formatDuration(data.end)}</div>
+                                        <div className="start-time">{formatDuration(data.start)}</div>
+                                        <div className={props.language === "fa" ? "data-text-fa" : "data-text-en"}>{data.text}</div>
                                     </div>
                                 );
                             }
@@ -120,7 +148,7 @@ const ArchiveFileAudio = (props) => {
                     }
                 </div>
             </SimpleBarReact>
-            <div className="footer">
+            <div className="footer-archive">
                 <div className="voice-bar">
                     <VoiceBar {...props} />
                 </div>
